f3625cd67bff28632004c74f7d73911ad4acc8ae
Esenwa Kachukwu Michael <michael.esenwa@gmail.com>
2025-01-16 20:40:55 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+struct Monomial {
+    coefficient: usize,
+    degree: usize,
+}
+
+impl Monomial {
+    fn new(coefficient: usize, degree: usize) -> Self {
+        Monomial {
+            coefficient,
+            degree,
+        }
+    }
+
+    fn evaluate(&self, x: usize) -> usize {
+        self.coefficient * x.pow(self.degree as u32)
+    }
+
+    fn degree(&self) -> usize {
+        self.degree
+    }
+}
+
+fn main() {
+    let poly = Monomial::new(3, 2);
+    let result = poly.evaluate(5);
+    println!("Polynomial at x = 5 is: {}", result);
+    println!("Degree of the polynomial is: {}", poly.degree());
+}
